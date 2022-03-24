700abbf55dbdac27ca87ca3531c44aeb605952c1
BuildTools <unconfigured@null.spigotmc.org>
2022-03-24 18:44:54 +0100

    final commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static("client"));
+app.use(express.static("public"));
 
 app.use(express.json());
 
